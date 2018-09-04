2cde9e82df5afb7ac134e5c645b4e040afc4d3f2
Arti M <artismarti@gmail.com>
2018-09-04 22:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+
+let katzDeliLine =[]
+let newNumber = 0
+
+function takeANumber(katzDeliLine){
+  newNumber += 1
+  katzDeliLine.push(newNumber)
+  return `Welcome! You're ticket number ${newNumber}`
+}
+
+
+// takeANumber(katzDeliLine) => "Welcome! You're ticket number 1!" [1]
+// takeANumber(katzDeliLine) => "Welcome! You're ticket number 2!" [1, 2]
+
+// nowServing(katzDeliLine) => "Currently serving: #1" [2]
+// nowServing(katzDeliLine) => "Currently serving: #2" []
+
+// takeANumber(katzDeliLine) => "Welcome! You're ticket number #3!" [3]
+
+
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
